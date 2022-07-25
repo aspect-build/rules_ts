e9ffd2ae9c795c1e2e3e891dbdb145f90e3e3ba7
Jason Bedard <jason+github@jbedard.ca>
2022-07-25 10:53:10 -0700

    test: add example of ts_project .d.ts source (#98)

---
+++ examples/dts_lib/importer.ts
@@ -0,0 +1,5 @@
+import { A as RelA } from './index';
+import { A as AbsA } from '@myorg/dts_lib';
+
+export const aa: AbsA = 42;
+export const a: RelA = 42;
--- examples/simple/foo.ts
+++ examples/simple/foo.ts
@@ -1,8 +1,11 @@
 import num from '@myorg/js_lib'
+import { A } from '@myorg/dts_lib'
 import { format } from 'date-fns'
 
-export const a: string = `number: ${num}, date: ${format(
+export const a: A = 123;
+export const b: string = `number: ${num}, date: ${format(
     new Date(2014, 1, 11),
     'MM/dd/YYYY'
 )}`
-console.log(a)
+
+console.log(a, b)
