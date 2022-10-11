2e1764ba1c451a7e3adf0990930fc6312997f827
Sahin Yort <thesayyn@gmail.com>
2022-10-11 21:30:51 +0300

    fix: report failedLookups twice (#169)

---
+++ e2e/worker_workspace/test.sh
@@ -0,0 +1,12 @@
+# asserts that in a cold start missing deps are reported correctly and succeeds in a subsequent build
+# where missing deps are in place.
+# Discovered in: https://github.com/aspect-build/bazel-examples/tree/js_build_file_generation
+set -x
+bazel clean
+
+buildozer "remove deps //apps/triad/pivot //apps/triad/vibe" //apps/triad:triad_ts
+bazel build //apps/triad:triad_ts
+
+set -o errexit
+buildozer "add deps //apps/triad/pivot //apps/triad/vibe" //apps/triad:triad_ts
+bazel build //apps/triad:triad_ts
