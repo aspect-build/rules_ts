5cfc997abd07d6a6adac7716f3b9d346febc24e9
Alex Eagle <alex@aspect.dev>
2022-11-04 12:15:47 -0700

    chore: updates for bzlmod launch
    - we are broken on windows, https://github.com/aspect-build/rules_ts/issues/228
    - add the snippet so our next release shows how to use from BCR

--- .github/workflows/workspace_snippet.sh
+++ .github/workflows/workspace_snippet.sh
@@ -10,7 +10,34 @@ PREFIX="rules_ts-${TAG:1}"
 SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
 
 cat << EOF
-WORKSPACE snippet:
+## Using [Bzlmod] with Bazel 6:
+
+Add to your \`MODULE.bazel\` file:
+
+\`\`\`starlark
+bazel_dep(name = "aspect_rules_ts", version = "${TAG:1}")
+
+rules_ts_ext = use_extension(
+    "@aspect_rules_ts//ts:extensions.bzl",
+    "ext",
+    dev_dependency = True,
+)
+
+rules_ts_ext.deps()
+
+use_repo(rules_ts_ext, "npm_typescript")
+
+use_repo(rules_ts_ext, "npm_google_protobuf")
+
+use_repo(rules_ts_ext, "npm_at_bazel_worker")
+\`\`\`
+
+[Bzlmod]: https://bazel.build/build/bzlmod
+
+## Using WORKSPACE
+
+Paste this snippet into your \`WORKSPACE\` file:
+
 \`\`\`starlark
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 http_archive(
