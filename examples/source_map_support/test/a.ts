aspect-build/rules_ts

commit ad69d9358f47fba10ae46b3e9d4661ec14aef5a3

refactor: improve worker correctness and add observability (#264)
* refactor: write debug info to worker log

* fix state bugs

* fix ci

* add fstree debugging

* fix ts_project

* add tests and fix sourcemap roots

* add new tests and remove old e2e/worker_*

* cleanup

* fix fstree tests

* handle tsconfig changes

* fix ci

* load system wide libraries

* fix tests

* remove protobuf repos

* address changes

* fix outputs

* expand symlinks and notify at last

* fix tests

* fix ci

* remove extra debug

e2e/worker/transitive_closure/BUILD.bazel
@@ -1,14 +1,0 @@
-load("@aspect_rules_ts//ts:defs.bzl", "ts_project")
-
-load("@npm//:defs.bzl", "npm_link_all_packages")
-npm_link_all_packages(name = "node_modules")
-
-ts_project(
-    name = "transitive_closure",
-    srcs = ["index.ts"],
-    deps = [
-        ":node_modules/@transitive_closure/a",
-    ],
-    tsconfig = "//:tsconfig",
-    tags = ["manual"]
-)

e2e/worker_workspace/BUILD.bazel
@@ -1,18 +1,0 @@
-load("@npm//:defs.bzl", "npm_link_all_packages")
-load("@aspect_rules_ts//ts:defs.bzl", "ts_config")
-load("@bazel_skylib//rules:build_test.bzl", "build_test")
-
-npm_link_all_packages()
-
-ts_config(
-    name = "tsconfig",
-    src = "tsconfig.json",
-    visibility = ["//visibility:public"],
-)
-
-build_test(
-    name = "test",
-    targets = [
-        "//apps/triad:triad_ts"
-    ],
-)

e2e/worker_workspace/apps/triad/BUILD.bazel
@@ -1,35 +1,0 @@
-load("@aspect_rules_js//js:defs.bzl", "js_binary", "js_library")
-load("@aspect_rules_ts//ts:defs.bzl", "ts_project")
-load("@npm//:defs.bzl", "npm_link_all_packages")
-
-npm_link_all_packages()
-
-ts_project(
-    name = "triad_ts",
-    srcs = [
-        "main.ts",
-    ],
-    declaration = True,
-    tsconfig = "//:tsconfig",
-    deps = [
-        "//apps/triad/pivot",
-        "//apps/triad/vibe",
-    ],
-    visibility = ["//visibility:public"]
-)
-
-js_library(
-    name = "triad_lib",
-    srcs = [
-        "package.json",
-        ":triad_ts",
-    ],
-)
-
-js_binary(
-    name = "triad",
-    data = [
-        ":triad_lib",
-    ],
-    entry_point = "main.js",
-)

e2e/worker_workspace/apps/triad/pivot/BUILD.bazel
@@ -1,10 +1,0 @@
-load("@aspect_rules_js//js:defs.bzl", "js_library")
-
-js_library(
-    name = "pivot",
-    srcs = [
-        "index.d.ts",
-        "index.js",
-    ],
-    visibility = ["//visibility:public"],
-)

e2e/worker_workspace/apps/triad/vibe/BUILD.bazel
@@ -1,23 +1,0 @@
-load("@aspect_rules_ts//ts:defs.bzl", "ts_project")
-load("@aspect_rules_js//js:defs.bzl", "js_library")
-
-ts_project(
-    name = "vibe_ts",
-    srcs = [
-        "index.ts",
-    ],
-    declaration = True,
-    tsconfig = "//:tsconfig",
-    deps = [
-        "//apps/triad:node_modules/@example/iris",
-        "//libs/rune/asap",
-    ],
-)
-
-js_library(
-    name = "vibe",
-    srcs = [
-        ":vibe_ts",
-    ],
-    visibility = ["//visibility:public"],
-)

e2e/worker_workspace/libs/rune/BUILD.bazel
@@ -1,3 +1,0 @@
-load("@npm//:defs.bzl", "npm_link_all_packages")
-
-npm_link_all_packages()

e2e/worker_workspace/libs/rune/asap/BUILD.bazel
@@ -1,10 +1,0 @@
-load("@aspect_rules_js//js:defs.bzl", "js_library")
-
-js_library(
-    name = "asap",
-    srcs = [
-        "index.d.ts",
-        "index.js",
-    ],
-    visibility = ["//visibility:public"],
-)

e2e/worker_workspace/libs/rune/rebus/BUILD.bazel
@@ -1,41 +1,0 @@
-load("@aspect_rules_js//js:defs.bzl", "js_library")
-load("@aspect_rules_ts//ts:defs.bzl", "ts_project")
-
-ts_project(
-    name = "rebus_ts",
-    srcs = [
-        "index.ts",
-    ],
-    declaration = True,
-    tsconfig = "//:tsconfig",
-)
-
-js_library(
-    name = "rebus",
-    srcs = [
-        ":rebus_ts",
-    ],
-    visibility = ["//visibility:public"],
-)
-
-ts_project(
-    name = "rebus_test_ts",
-    srcs = [
-        "index.spec.ts",
-    ],
-    declaration = True,
-    tsconfig = "//:tsconfig",
-    deps = [
-        ":rebus",
-        "//:node_modules/@types/jest",
-        "//:node_modules/@types/node",
-    ],
-)
-
-js_library(
-    name = "rebus_test_lib",
-    srcs = [
-        ":rebus",
-        ":rebus_test_ts",
-    ],
-)

commit 26b4f40809a34a5883a6465165007021510988fa

source-map-support regressed in ad69d9358f47fba10ae46b3e9d4661ec14aef5a3

examples/pnpm-lock.yaml
@@ -14,6 +14,7 @@ specifiers:
   '@types/node': 18.11.9
   date-fns: 2.29.3
   rxjs: 7.5.7
+  source-map-support: ^0.5.21
   typescript: 4.8.4
   zone.js: 0.12.0
 
@@ -31,6 +32,7 @@ dependencies:
   '@types/node': 18.11.9
   date-fns: 2.29.3
   rxjs: 7.5.7
+  source-map-support: 0.5.21
   typescript: 4.8.4
   zone.js: 0.12.0
 
@@ -535,6 +537,10 @@ packages:
       update-browserslist-db: 1.0.10_browserslist@4.21.4
     dev: false
 
+  /buffer-from/1.1.2:
+    resolution: {integrity: sha512-E+XQCRwSbaaiChtv6k6Dwgc+bx+Bs6vuKJHHl5kox/BaKbhiXzqQOwK4cO22yElGp2OCmjwVhT3HmxgyPGnJfQ==}
+    dev: false
+
   /caniuse-lite/1.0.30001435:
     resolution: {integrity: sha512-kdCkUTjR+v4YAJelyiDTqiu82BDr4W4CP5sgTA0ZBmqn30XfS2ZghPLMowik9TPhS+psWJiUNxsqLyurDbmutA==}
     dev: false
@@ -872,6 +878,18 @@ packages:
   /slash/2.0.0:
     resolution: {integrity: sha512-ZYKh3Wh2z1PpEXWr0MpSBZ0V6mZHAQfYevttO11c51CaWjGTaadiKZ+wVt1PbMlDV5qhMFslpZCemhwOK7C89A==}
     engines: {node: '>=6'}
+    dev: false
+
+  /source-map-support/0.5.21:
+    resolution: {integrity: sha512-uBHU3L3czsIyYXKX88fdrGovxdSCoTGDRZ6SYXtSRxLZUzHg5P/66Ht6uoUlHu9EZod+inXhKo3qQgwXUT/y1w==}
+    dependencies:
+      buffer-from: 1.1.2
+      source-map: 0.6.1
+    dev: false
+
+  /source-map/0.6.1:
+    resolution: {integrity: sha512-UjgapumWlbMhkBgzT7Ykc5YXUT46F0iKu8SGXq0bcwP5dz/h0Plj6enJqjz1Zbq2l5WaqYnrVbwWOWMyF3F47g==}
+    engines: {node: '>=0.10.0'}
     dev: false
 
   /sourcemap-codec/1.4.8:

examples/source_map_support/BUILD.bazel
@@ -1,0 +1,11 @@
+load("@aspect_bazel_lib//lib:copy_to_bin.bzl", "copy_to_bin")
+
+exports_files([
+    "defs.bzl",
+])
+
+copy_to_bin(
+    name = "stack-trace-support",
+    srcs = ["stack-trace-support.js"],
+    visibility = ["//visibility:public"],
+)

examples/source_map_support/defs.bzl
@@ -1,0 +1,28 @@
+"""
+Macro wrappers around rules_js's `js_binary` and `js_test` that improve the DX of stack traces by automatically
+registering source-map-support and removing the runfiles directory prefix.
+
+Use them wherever you would use rules_js's `js_binary` and `js_test`.
+"""
+
+load("@aspect_rules_js//js:defs.bzl", _js_binary = "js_binary", _js_test = "js_test")
+
+def js_binary(data = [], node_options = [], **kwargs):
+    _js_binary(
+        data = [
+            "//examples:node_modules/source-map-support",
+            "//examples/source_map_support:stack-trace-support",
+        ] + data,
+        node_options = ["--require", "$$RUNFILES/aspect_rules_ts/examples/source_map_support/stack-trace-support"] + node_options,
+        **kwargs
+    )
+
+def js_test(data = [], node_options = [], **kwargs):
+    _js_test(
+        data = [
+            "//examples:node_modules/source-map-support",
+            "//examples/source_map_support:stack-trace-support",
+        ] + data,
+        node_options = ["--require", "$$RUNFILES/aspect_rules_ts/examples/source_map_support/stack-trace-support"] + node_options,
+        **kwargs
+    )

examples/source_map_support/stack-trace-support.js
@@ -1,0 +1,42 @@
+// See defs.bzl for where this is used and what it does.
+
+require('source-map-support/register')
+
+let basePath = process.env.RUNFILES
+  ? `${process.env.RUNFILES}/${process.env.JS_BINARY__WORKSPACE}`
+  : process.cwd()
+
+if (!basePath.endsWith('/')) {
+  basePath = basePath + '/'
+}
+
+/*
+Before:
+    Error: test
+        at foo (/private/var/tmp/_bazel_john/67beefda950d56283b98d96980e6e332/execroot/figma/bazel-out/darwin_arm64-fastbuild/bin/bazel/js/test/stack_trace_support.sh.runfiles/figma/bazel/js/test/b.js:2:11)
+        at Object.<anonymous> (/private/var/tmp/_bazel_john/67beefda950d56283b98d96980e6e332/execroot/figma/bazel-out/darwin_arm64-fastbuild/bin/bazel/js/test/stack_trace_support.sh.runfiles/figma/bazel/js/test/a.js:4:1)
+        ...
+
+After:
+    Error: test
+        at foo (bazel/js/test/b.ts:2:9)
+        at Object.<anonymous> (bazel/js/test/a.ts:5:1)
+        ...
+*/
+
+const basePathRegex = new RegExp(
+  `(at | \\()${basePath
+    .replace(/\\/g, '/')
+    // Escape regex meta-characters.
+    .replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+    .replace(/-/g, '\\x2d')}`,
+  'g',
+)
+
+const prepareStackTrace = Error.prepareStackTrace
+Error.prepareStackTrace = function (error, stack) {
+  return prepareStackTrace(error, stack)
+    .split('\n')
+    .map((line) => line.replace(basePathRegex, '$1'))
+    .join('\n')
+}

examples/source_map_support/test/BUILD.bazel
@@ -1,0 +1,33 @@
+load("//ts:defs.bzl", "ts_project")
+load("//examples/source_map_support:defs.bzl", "js_test")
+
+ts_project(
+    name = "ts",
+    srcs = [
+        "a.ts",
+        "b.ts",
+    ],
+    source_map = True,
+    tsconfig = {
+        "compilerOptions": {
+            "types": ["node"],
+            "sourceMap": True,
+        },
+    },
+    deps = [
+        "//examples:node_modules/@types/node",
+    ],
+)
+
+js_test(
+    name = "stack_trace_support_test",
+    data = [":ts"],
+    entry_point = ":a.js",
+)
+
+js_test(
+    name = "stack_trace_support_with_chdir_test",
+    chdir = "examples",
+    data = [":ts"],
+    entry_point = ":a.js",
+)

examples/source_map_support/test/a.ts
@@ -1,0 +1,17 @@
+try {
+  require('./b')()
+} catch (e) {
+  const assert = require('assert')
+  const frames = e.stack
+    .split('\n')
+    .slice(1)
+    .map((s) => s.trim())
+  assert.deepEqual(
+    frames.filter((f) => f.includes('source_map_support/test/a')),
+    [`at Object.<anonymous> (examples/source_map_support/test/a.ts:2:17)`],
+  )
+  assert.deepEqual(
+    frames.filter((f) => f.includes('source_map_support/test/b')),
+    [`at foo (examples/source_map_support/test/b.ts:2:9)`],
+  )
+}

examples/source_map_support/test/b.ts
@@ -1,0 +1,3 @@
+module.exports = function foo() {
+  throw new Error('test')
+}