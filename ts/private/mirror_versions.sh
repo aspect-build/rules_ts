40f7329c6266fdd21316ed8dbe2d89f021af797a
Alex Eagle <alex@aspect.dev>
2023-11-09 10:29:29 -0800

    chore: add automation for TS version mirroring
    Based on https://github.com/aspect-build/rules_lint/blob/main/.github/workflows/mirror.yml

---
+++ ts/private/mirror_versions.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -o nounset -o errexit -o pipefail
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+JQ_FILTER=\
+'
+[
+    .versions[]
+    | select(.version | test("^[0-9.]+$"))
+    | {key: .version, value: .dist.integrity}
+] | from_entries
+'
+
+NEW=$(mktemp)
+sed '/TOOL_VERSIONS =/Q' $SCRIPT_DIR/versions.bzl > $NEW
+echo -n "TOOL_VERSIONS = " >> $NEW
+curl --silent https://registry.npmjs.org/typescript | jq "$JQ_FILTER" >> $NEW
+
+cp $NEW $SCRIPT_DIR/versions.bzl
