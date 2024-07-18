7cf3267396177c050abb8b5fc0c9aa270eaef800
Jason Bedard <jason+github@jbedard.ca>
2024-07-17 22:10:00 -0700

    test: npm packages using js_library

--- examples/simple/foo.ts
+++ examples/simple/foo.ts
@@ -1,11 +1,13 @@
 import num from '@myorg/js_lib'
+import num2 from '@myorg/js_lib_pkg'
 import { A } from '@myorg/dts_lib'
 import { format } from 'date-fns'
 
-export const a: A = 123;
+export const a: A = 123
 export const b: string = `number: ${num}, date: ${format(
     new Date(2014, 1, 11),
     'MM/dd/YYYY'
 )}`
+export const c: string = `number: ${num2}`
 
-console.log(a, b)
+console.log(a, b, c)
