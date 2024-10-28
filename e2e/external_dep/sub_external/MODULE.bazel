aspect-build/rules_ts

commit 85d969ee603f159dda3502deceb15b02cd9d1135

perf: remove unnecessary tsconfig relative_file computation (#687)

ts/private/ts_config.bzl
@@ -137,10 +137,15 @@ def _write_tsconfig_rule(ctx):
 
     filtered_files = _filter_input_files(ctx.files.files, ctx.attr.allow_js, ctx.attr.resolve_json_module)
     if filtered_files:
-        content = content.replace(
-            "\"__files__\"",
-            str([relative_file(f.short_path, ctx.outputs.out.short_path) for f in filtered_files]),
-        )
+        # Update file paths to be relative to the tsconfig file, including a ./ prefix
+        # to ensure paths are all relative to the config file.
+        package_prefix = ctx.label.package + "/"
+        filtered_files = [
+            "./" + f.short_path.removeprefix(package_prefix)
+            for f in filtered_files
+        ]
+
+        content = content.replace("\"__files__\"", str(filtered_files))
     ctx.actions.write(
         output = ctx.outputs.out,
         content = content,

commit 6df0fe6d1ac1a08da9d2730da340dda454575a84

Ensuring package_prefix is correct for external workspaces (#726)
Fix after breakage from https://github.com/aspect-build/rules_ts/pull/687

### Changes are visible to end-users: no

### Test plan

- Covered by existing test cases
- New test cases added

ts/private/ts_config.bzl
@@ -140,6 +140,11 @@ def _write_tsconfig_rule(ctx):
     # Update file paths to be relative to the tsconfig file, including a ./ prefix
     # to ensure paths are all relative to the config file.
     package_prefix = ctx.label.package + "/"
+
+    # If the target is inside another workspace, we will need to also add the workspace
+    # root to the prefix.
+    if (len(ctx.label.repo_name) > 0):
+        package_prefix = "../{}/{}".format(ctx.label.repo_name, package_prefix)
     filtered_files = [
         "./" + f.short_path.removeprefix(package_prefix)
         for f in filtered_files

e2e/external_dep/.bazelignore
@@ -1,2 +1,3 @@
 app/
 node_modules/
+sub_external/

e2e/external_dep/BUILD
@@ -38,3 +38,28 @@ build_test(
         "lib.ts56rc",
     ],
 )
+
+ts_project(
+    name = "external-deps",
+    srcs = ["lib.ts"],
+    composite = True,
+    out_dir = "external-deps",
+    visibility = ["//visibility:public"],
+    deps = [
+        "@sub_external//:lib1",
+        "@sub_external//:tsconfig-dict",
+        "@sub_external//subdir:lib1",
+        "@sub_external//subdir:tsconfig-dict",
+    ],
+)
+
+build_test(
+    name = "test-external",
+    targets = [
+        ":external-deps",
+        "@sub_external//:lib1",
+        "@sub_external//:tsconfig-dict",
+        "@sub_external//subdir:lib1",
+        "@sub_external//subdir:tsconfig-dict",
+    ],
+)

e2e/external_dep/MODULE.bazel
@@ -36,3 +36,9 @@ rules_ts_ext.deps(
     ts_version = "5.6.1-rc",
 )
 use_repo(rules_ts_ext, "npm_typescript", "npm_typescript-56rc", "npm_typescript3")
+
+bazel_dep(name = "sub_external", version = "0.0.0", dev_dependency = True)
+local_path_override(
+    module_name = "sub_external",
+    path = "./sub_external",
+)

e2e/external_dep/WORKSPACE
@@ -28,6 +28,12 @@ rules_ts_dependencies(
     ts_version = "5.6.1-rc",
 )
 
+# NOTE: only required on non-bzlmod where the sub_external/WORKSPACE is not loaded to declare npm_typescript2
+rules_ts_dependencies(
+    name = "npm_typescript2",
+    ts_version = LATEST_TYPESCRIPT_VERSION,
+)
+
 load("@aspect_rules_js//js:repositories.bzl", "rules_js_dependencies")
 
 rules_js_dependencies()
@@ -48,3 +54,8 @@ npm_translate_lock(
 load("@npm//:repositories.bzl", "npm_repositories")
 
 npm_repositories()
+
+local_repository(
+    name = "sub_external",
+    path = "./sub_external",
+)

e2e/external_dep/sub_external/.bazelrc
@@ -1,0 +1,18 @@
+# Import Aspect bazelrc presets
+try-import %workspace%/../../../.aspect/bazelrc/bazel7.bazelrc
+import %workspace%/../../../.aspect/bazelrc/convenience.bazelrc
+import %workspace%/../../../.aspect/bazelrc/correctness.bazelrc
+import %workspace%/../../../.aspect/bazelrc/debug.bazelrc
+import %workspace%/../../../.aspect/bazelrc/javascript.bazelrc
+import %workspace%/../../../.aspect/bazelrc/performance.bazelrc
+
+### YOUR PROJECT SPECIFIC OPTIONS GO HERE ###
+
+common --@aspect_rules_ts//ts:skipLibCheck=honor_tsconfig
+common --@aspect_rules_ts//ts:default_to_tsc_transpiler
+
+# Load any settings & overrides specific to the current user from `.aspect/bazelrc/user.bazelrc`.
+# This file should appear in `.gitignore` so that settings are not shared with team members. This
+# should be last statement in this config so the user configuration is able to overwrite flags from
+# this file. See https://bazel.build/configure/best-practices#bazelrc-file.
+try-import %workspace%/../../../.aspect/bazelrc/user.bazelrc

e2e/external_dep/sub_external/BUILD.bazel
@@ -1,0 +1,23 @@
+"""Tests a `ts_project()` in one workspace being used in another."""
+
+load("//:ts.bzl", "ts_project")
+
+ts_project(
+    name = "lib1",
+    srcs = ["sub-lib.ts"],
+    declaration = True,
+    out_dir = "out1",
+    visibility = ["//visibility:public"],
+)
+
+ts_project(
+    name = "tsconfig-dict",
+    srcs = ["sub-lib.ts"],
+    tsconfig = {
+        "compilerOptions": {
+            "declaration": True,
+            "outDir": "tsconfig-dict",
+        },
+    },
+    visibility = ["//visibility:public"],
+)

e2e/external_dep/sub_external/MODULE.bazel
@@ -1,0 +1,18 @@
+module(
+    name = "sub_external",
+    version = "0.0.0",
+)
+
+bazel_dep(name = "aspect_rules_ts", version = "0.0.0")
+local_path_override(
+    module_name = "aspect_rules_ts",
+    path = "../../..",
+)
+
+rules_ts_ext = use_extension("@aspect_rules_ts//ts:extensions.bzl", "ext")
+
+# Use an alternate npm_typescript name to workaround https://github.com/aspect-build/rules_ts/issues/483
+rules_ts_ext.deps(
+    name = "npm_typescript2",
+)
+use_repo(rules_ts_ext, "npm_typescript2")

e2e/external_dep/sub_external/WORKSPACE
@@ -1,0 +1,11 @@
+local_repository(
+    name = "aspect_rules_ts",
+    path = "../../..",
+)
+
+load("@aspect_rules_ts//ts:repositories.bzl", "LATEST_TYPESCRIPT_VERSION", "rules_ts_dependencies")
+
+rules_ts_dependencies(
+    name = "npm_typescript2",
+    ts_version = LATEST_TYPESCRIPT_VERSION,
+)

e2e/external_dep/sub_external/sub-lib.ts
@@ -1,0 +1,1 @@
+console.log("Hello");

e2e/external_dep/sub_external/subdir/BUILD.bazel
@@ -1,0 +1,23 @@
+"""Tests a `ts_project()` in one workspace being used in another."""
+
+load("//:ts.bzl", "ts_project")
+
+ts_project(
+    name = "lib1",
+    srcs = ["sub-lib.ts"],
+    declaration = True,
+    out_dir = "out1",
+    visibility = ["//visibility:public"],
+)
+
+ts_project(
+    name = "tsconfig-dict",
+    srcs = ["sub-lib.ts"],
+    tsconfig = {
+        "compilerOptions": {
+            "declaration": True,
+            "outDir": "tsconfig-dict",
+        },
+    },
+    visibility = ["//visibility:public"],
+)

e2e/external_dep/sub_external/subdir/sub-lib.ts
@@ -1,0 +1,1 @@
+console.log("Hello");

e2e/external_dep/sub_external/ts.bzl
@@ -1,0 +1,14 @@
+"""
+Util for hiding ts_project() workaround for  https://github.com/aspect-build/rules_ts/issues/483
+"""
+
+load("@aspect_rules_ts//ts:defs.bzl", _ts_project = "ts_project")
+
+def ts_project(name, **kwargs):
+    _ts_project(
+        name = name,
+        tsc = "@npm_typescript2//:tsc",
+        tsc_worker = "@npm_typescript2//:tsc_worker",
+        validator = "@npm_typescript2//:validator",
+        **kwargs
+    )