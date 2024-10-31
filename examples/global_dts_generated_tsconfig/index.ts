fd2b883c551a8877467b5c1af26bea9fed59ce1d
Jason Bedard <jason+github@jbedard.ca>
2024-10-31 12:15:50 -0700

    fix: ts_project declaration sources from other directories
    Fix https://github.com/aspect-build/rules_ts/issues/727

--- examples/global_dts_generated_tsconfig/index.ts
+++ examples/global_dts_generated_tsconfig/index.ts
@@ -1,1 +1,11 @@
-myGlobalFunction(42)
+;(typeof globalThis !== 'undefined'
+    ? globalThis
+    : (window as any)
+).myGlobalResult = myGlobalFunction(42)
+
+export {}
+
+// A definition within a .ts file that must be compiled.
+declare global {
+    const myGlobalResult: string
+}
