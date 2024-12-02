aspect-build/rules_ts

commit cf8472297fa4d672515d9f083ff78f82058ff3db

fix: respect ts_project root_dir and out_dir for assets
Fix https://github.com/aspect-build/rules_ts/issues/436

examples/assets/BUILD.bazel
@@ -1,0 +1,82 @@
+load("@aspect_rules_ts//ts:defs.bzl", "ts_config", "ts_project")
+load("@bazel_skylib//rules:build_test.bzl", "build_test")
+
+ts_config(
+    name = "config",
+    src = "tsconfig.json",
+    deps = [
+        "types/typings.d.ts",
+        "//examples:node_modules/@types/node",
+    ],
+)
+
+ts_project(
+    name = "ts",
+    srcs = [
+        "src/index.ts",
+    ],
+    assets = [
+        "src/styles.css",
+    ],
+    tsconfig = ":config",
+)
+
+ts_project(
+    name = "ts-out",
+    srcs = [
+        "src/index.ts",
+    ],
+    assets = [
+        "src/styles.css",
+    ],
+    extends = ":config",
+    out_dir = "out",
+    tsconfig = {"compilerOptions": {"outDir": "out"}},
+)
+
+ts_project(
+    name = "ts-root",
+    srcs = [
+        "src/index.ts",
+    ],
+    assets = [
+        "src/styles.css",
+    ],
+    root_dir = "src",
+    tsconfig = ":config",
+)
+
+ts_project(
+    name = "ts-root-out",
+    srcs = [
+        "src/index.ts",
+    ],
+    assets = [
+        "src/styles.css",
+    ],
+    out_dir = "root-out",
+    root_dir = "src",
+    tsconfig = ":config",
+)
+
+build_test(
+    name = "test",
+    targets = [
+        # Defaults of no root/out_dir
+        ":ts",
+        ":src/index.js",
+        ":src/styles.css",
+        # out_dir
+        ":ts-out",
+        ":out/src/index.js",
+        ":out/src/styles.css",
+        # root_dir
+        ":ts-root",
+        ":index.js",
+        ":styles.css",
+        # root_dir + out_dir
+        ":ts-root-out",
+        ":root-out/index.js",
+        ":root-out/styles.css",
+    ],
+)

ts/private/ts_project.bzl
@@ -1,5 +1,6 @@
 "ts_project rule"
 
+load("@aspect_bazel_lib//lib:copy_file.bzl", "copy_file_action")
 load("@aspect_bazel_lib//lib:copy_to_bin.bzl", "COPY_FILE_TO_BIN_TOOLCHAINS", "copy_file_to_bin_action", "copy_files_to_bin_actions")
 load("@aspect_bazel_lib//lib:paths.bzl", "to_output_relative_path")
 load("@aspect_bazel_lib//lib:platform_utils.bzl", "platform_utils")
@@ -141,6 +142,14 @@ See https://github.com/aspect-build/rules_ts/issues/361 for more details.
     tsconfig_inputs = copy_files_to_bin_actions(ctx, _validate_lib.tsconfig_inputs(ctx).to_list())
     inputs.extend(tsconfig_inputs)
 
+    assets_outs = []
+    for a in ctx.files.assets:
+        a_path = _lib.relative_to_package(a.short_path, ctx)
+        a_out = _lib.to_out_path(a_path, ctx.attr.out_dir, ctx.attr.root_dir)
+        asset = ctx.actions.declare_file(a_out)
+        copy_file_action(ctx, a, asset)
+        assets_outs.append(asset)
+
     outputs = js_outs + map_outs + typings_outs + typing_maps_outs
     if ctx.outputs.buildinfo_out:
         arguments.add_all([
@@ -149,11 +158,11 @@ See https://github.com/aspect-build/rules_ts/issues/361 for more details.
         ])
         outputs.append(ctx.outputs.buildinfo_out)
 
-    output_sources = js_outs + map_outs + copy_files_to_bin_actions(ctx, ctx.files.assets)
+    output_sources = js_outs + map_outs + assets_outs
 
     # Add JS inputs that collide with outputs (see #250).
     #
-    # Unfortunately this duplicates logic in ts_lib._out_paths:
+    # Unfortunately this duplicates logic in ts_lib._to_js_out_paths:
     # files collide iff the following conditions are met:
     # - They are JS files (ext in [js, json])
     # - out_dir == root_dir

ts/private/BUILD.bazel
@@ -49,6 +49,7 @@ bzl_library(
         ":ts_config",
         ":ts_lib",
         ":ts_validate_options",
+        "@aspect_bazel_lib//lib:copy_file",
         "@aspect_bazel_lib//lib:copy_to_bin",
         "@aspect_bazel_lib//lib:paths",
         "@aspect_bazel_lib//lib:platform_utils",

examples/assets/src/index.ts
@@ -1,0 +1,3 @@
+import css from './styles.css'
+
+console.log(__dirname, css)

examples/assets/src/styles.css
@@ -1,0 +1,3 @@
+body {
+    background: red;
+}

examples/assets/types/typings.d.ts
@@ -1,0 +1,4 @@
+declare module '*.css' {
+    const cssModule: string
+    export default cssModule
+}

ts/defs.bzl
@@ -342,6 +342,8 @@ def ts_project(
     typings_out_dir = declaration_dir if declaration_dir else out_dir
     tsbuildinfo_path = ts_build_info_file if ts_build_info_file else name + ".tsbuildinfo"
 
+    assets_outs = _lib.calculate_assets_outs(assets, out_dir, root_dir)
+
     tsc_typings_outs = _lib.calculate_typings_outs(srcs, typings_out_dir, root_dir, declaration, composite, allow_js)
     tsc_typing_maps_outs = _lib.calculate_typing_maps_outs(srcs, typings_out_dir, root_dir, declaration_map, allow_js)
 
@@ -431,6 +433,7 @@ def ts_project(
         map_outs = tsc_map_outs,
         typings_outs = tsc_typings_outs,
         typing_maps_outs = tsc_typing_maps_outs,
+        assets_outs = assets_outs,
         buildinfo_out = tsbuildinfo_path if composite or incremental else None,
         emit_declaration_only = emit_declaration_only,
         tsc = tsc,

ts/private/ts_lib.bzl
@@ -144,6 +144,9 @@ OUTPUT_ATTRS = {
     "typings_outs": attr.output_list(
         doc = "Locations in bazel-out where tsc will write `.d.ts` files",
     ),
+    "assets_outs": attr.output_list(
+        doc = "Locations in bazel-out where ts_project will output asset files",
+    ),
 }
 
 def _join(*elements):
@@ -191,17 +194,32 @@ def _replace_ext(f, ext_map):
         return new_ext
     return None
 
-def _out_paths(srcs, out_dir, root_dir, allow_js, resolve_json_module, ext_map):
-    rootdir_replace_pattern = root_dir + "/" if root_dir else ""
+def _to_out_path(f, out_dir, root_dir):
+    if root_dir:
+        f = f.removeprefix(root_dir + "/")
+    if out_dir:
+        f = _join(out_dir, f)
+    return f
+
+def _to_js_out_paths(srcs, out_dir, root_dir, allow_js, resolve_json_module, ext_map):
     outs = []
     for f in srcs:
         if _is_ts_src(f, allow_js, resolve_json_module):
-            out = _join(out_dir, f[:f.rindex(".")].replace(rootdir_replace_pattern, "", 1) + _replace_ext(f, ext_map))
+            out = _to_out_path(f, out_dir, root_dir)
+            out = out[:out.rindex(".")] + _replace_ext(out, ext_map)
 
             # Don't declare outputs that collide with inputs
             # for example, a.js -> a.js
             if out != f:
                 outs.append(out)
+    return outs
+
+def _calculate_assets_outs(assets, out_dir = ".", root_dir = "."):
+    outs = []
+    for a in assets:
+        out = _to_out_path(a, out_dir, root_dir)
+        if out != a:
+            outs.append(out)
     return outs
 
 def _calculate_js_outs(srcs, out_dir = ".", root_dir = ".", allow_js = False, resolve_json_module = False, preserve_jsx = False, emit_declaration_only = False):
@@ -221,7 +239,7 @@ def _calculate_js_outs(srcs, out_dir = ".", root_dir = ".", allow_js = False, re
         exts[".jsx"] = ".jsx"
         exts[".tsx"] = ".jsx"
 
-    return _out_paths(srcs, out_dir, root_dir, allow_js, resolve_json_module, exts)
+    return _to_js_out_paths(srcs, out_dir, root_dir, allow_js, resolve_json_module, exts)
 
 def _calculate_map_outs(srcs, out_dir = ".", root_dir = ".", source_map = True, preserve_jsx = False, emit_declaration_only = False):
     if not source_map or emit_declaration_only:
@@ -237,7 +255,7 @@ def _calculate_map_outs(srcs, out_dir = ".", root_dir = ".", source_map = True, 
     if preserve_jsx:
         exts[".tsx"] = ".jsx.map"
 
-    return _out_paths(srcs, out_dir, root_dir, False, False, exts)
+    return _to_js_out_paths(srcs, out_dir, root_dir, False, False, exts)
 
 def _calculate_typings_outs(srcs, typings_out_dir, root_dir, declaration, composite, allow_js):
     if not (declaration or composite):
@@ -251,7 +269,7 @@ def _calculate_typings_outs(srcs, typings_out_dir, root_dir, declaration, compos
         ".cjs": ".d.cts",
     }
 
-    return _out_paths(srcs, typings_out_dir, root_dir, allow_js, False, exts)
+    return _to_js_out_paths(srcs, typings_out_dir, root_dir, allow_js, False, exts)
 
 def _calculate_typing_maps_outs(srcs, typings_out_dir, root_dir, declaration_map, allow_js):
     if not declaration_map:
@@ -265,7 +283,7 @@ def _calculate_typing_maps_outs(srcs, typings_out_dir, root_dir, declaration_map
         ".cjs": ".d.cts.map",
     }
 
-    return _out_paths(srcs, typings_out_dir, root_dir, allow_js, False, exts)
+    return _to_js_out_paths(srcs, typings_out_dir, root_dir, allow_js, False, exts)
 
 def _calculate_root_dir(ctx):
     return _join(
@@ -287,10 +305,12 @@ lib = struct(
     is_typings_src = _is_typings_src,
     is_ts_src = _is_ts_src,
     is_js_src = _is_js_src,
-    out_paths = _out_paths,
+    out_paths = _to_js_out_paths,
+    to_out_path = _to_out_path,
     calculate_js_outs = _calculate_js_outs,
     calculate_map_outs = _calculate_map_outs,
     calculate_typings_outs = _calculate_typings_outs,
     calculate_typing_maps_outs = _calculate_typing_maps_outs,
     calculate_root_dir = _calculate_root_dir,
+    calculate_assets_outs = _calculate_assets_outs,
 )

commit edf61ee40fa54f3584ae9e2c8c030673020a0bc6

fix: ensure assets get propagated (#742)
https://github.com/aspect-build/rules_ts/pull/533, in fixing the issue caused by https://github.com/aspect-build/rules_ts/pull/493, also omitted adding assets that shouldn't be copied to the `JsInfo` provider. As a result, downstream rules (such as `esbuild`) will fail to find the file because it never collected it.

examples/assets/BUILD.bazel
@@ -1,5 +1,6 @@
 load("@aspect_bazel_lib//lib:copy_to_directory.bzl", "copy_to_directory")
 load("@aspect_bazel_lib//lib:testing.bzl", "assert_outputs")
+load("@aspect_rules_js//js:defs.bzl", "js_test")
 load("@aspect_rules_ts//ts:defs.bzl", "ts_config", "ts_project")
 load("@bazel_skylib//rules:build_test.bzl", "build_test")
 
@@ -42,6 +43,34 @@ ts_project(
     extends = ":config",
     out_dir = "out",
     tsconfig = {"compilerOptions": {"outDir": "out"}},
+)
+
+ts_project(
+    name = "ts-runtime-assets",
+    srcs = ["src/index-runtime.ts"],
+    assets = ["src/generated.json"],
+    tsconfig = ":config",
+)
+
+js_test(
+    name = "ts-runtime-assets_test",
+    data = [":ts-runtime-assets"],
+    entry_point = "src/index-runtime.js",
+)
+
+ts_project(
+    name = "ts-runtime-outdir-assets",
+    srcs = ["src/index-runtime.ts"],
+    assets = ["src/generated.json"],
+    extends = ":config",
+    out_dir = "out",
+    tsconfig = {"compilerOptions": {"outDir": "out"}},
+)
+
+js_test(
+    name = "ts-runtime-outdir-assets_test",
+    data = [":ts-runtime-outdir-assets"],
+    entry_point = "out/src/index-runtime.js",
 )
 
 filegroup(

examples/assets/src/index-runtime.ts
@@ -1,0 +1,11 @@
+/**
+ * This testcase exists to verify assets get propagated correctly. It
+ * intentionally does not use `import` or `require` for the generated asset to
+ * more closely mimic what a downstream rule (eg. a bundler) would do.
+ */
+import * as fs from 'fs'
+import * as path from 'path'
+
+const data = fs.readFileSync(path.join(__dirname, 'generated.json'))
+
+console.log(__dirname, data)

ts/private/ts_project.bzl
@@ -167,6 +167,8 @@ See https://github.com/aspect-build/rules_ts/issues/361 for more details.
             asset = ctx.actions.declare_file(a_out)
             copy_file_action(ctx, a, asset)
             assets_outs.append(asset)
+        else:
+            assets_outs.append(a)
 
     outputs = js_outs + map_outs + typings_outs + typing_maps_outs
     if ctx.outputs.buildinfo_out: