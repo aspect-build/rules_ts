6b77bca92f2c1d79fe79eb78c0bb515cb6efae92
Alex Eagle <alex@aspect.dev>
2025-12-17 09:23:37 -0800

    another repro #476
    #819 created a changed proto but it wasn't imported anywhere

--- examples/proto_grpc/main.ts
+++ examples/proto_grpc/main.ts
@@ -1,7 +1,9 @@
 import { LogMessageSchema } from './logger_pb.js'
+import { EmptySchema } from './stripping/s_pb.js'
 import { create, fromBinary, toBinary } from '@bufbuild/protobuf'
 
 let msg = create(LogMessageSchema, { message: 'hello world' })
+create(EmptySchema, {})
 
 // Reference the inherited `.toBinary()` to ensure types from transitive types are included.
 msg = fromBinary(LogMessageSchema, toBinary(LogMessageSchema, msg))
