90e15630a22b8333785b33a3ce002c3520d274e0
Alex Eagle <alex@aspect.dev>
2026-01-30 07:58:20 -0800

    refactor: publish three stardocs to registry

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -13,6 +13,16 @@ ARCHIVE="rules_ts-$TAG.tar.gz"
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip >$ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
+# Add generated API docs to the release
+# see https://github.com/bazelbuild/bazel-central-registry/blob/main/docs/stardoc.md
+docs="$(mktemp -d)"
+targets="$(mktemp)"
+bazel --output_base="$docs" query --output=label --output_file="$targets" 'kind("starlark_doc_extract rule", //...)'
+bazel --output_base="$docs" build --target_pattern_file="$targets"
+tar --create --auto-compress \
+	--directory "$(bazel --output_base="$docs" info bazel-bin)" \
+	--file "$GITHUB_WORKSPACE/${ARCHIVE%.tar.gz}.docs.tar.gz" .
+
 cat <<EOF
 ## Using [Bzlmod]:
 
